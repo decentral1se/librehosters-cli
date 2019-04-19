dd20cb54019c791422432c34a9f0656f454df6f8
Luke Murphy <lukewm@riseup.net>
2019-04-19 23:41:25 +0200

    Improve on the versioning tests
    Signed-off-by: Luke Murphy <lukewm@riseup.net>

--- test/test_version.py
+++ test/test_version.py
@@ -1,7 +1,17 @@
 """Version test module."""
 
 
-def test_version_generates():
-    from librehosters_cli.__init__ import __version__
+def test_version_fails_gracefully(mocker):
+    target = 'pkg_resources.get_distribution'
+    with mocker.patch(target, side_effect=Exception()):
+        from librehosters_cli.__init__ import __version__
 
-    assert __version__ != 'unknown'
+        assert __version__ == 'unknown'
+
+
+def test_version_generates_successfully(runner):
+    from librehosters_cli.__main__ import main
+
+    result = runner.invoke(main, ['--version'])
+    assert result.exit_code == 0
+    assert 'unknown' not in result.output
